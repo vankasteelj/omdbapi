59b03185b3a18d48994b8f2cd9c3adef6908a222
vankasteelj <vankasteelj@gmail.com>
2023-05-07 21:50:07 +0200

    re-indent

--- omdbapi.js
+++ omdbapi.js
@@ -1,15 +1,15 @@
 'use strict';
 
 module.exports = class OMDB {
-    constructor(apiKey) {
-        this.utils = new (require('./utils.js'))(apiKey);
-    }
+  constructor(apiKey) {
+    this.utils = new (require('./utils.js'))(apiKey);
+  }
 
-    search(opts) {
-        return this.utils.get(opts, 'search');
-    }
+  search(opts) {
+    return this.utils.get(opts, 'search');
+  }
 
-    get(opts) {
-        return this.utils.get(opts, 'get');
-    }
+  get(opts) {
+    return this.utils.get(opts, 'get');
+  }
 };
