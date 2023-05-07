aa2664ddcb616e9ed93080fbac0cf4cfbf358fed
vankasteelj <vankasteelj@gmail.com>
2023-05-07 21:53:49 +0200

    standard utils.js

--- utils.js
+++ utils.js
@@ -1,84 +1,84 @@
 module.exports = class UTILS {
 
   constructor(apiKey) {
-    if (!apiKey) throw new Error('No apiKey provided');
+    if (!apiKey) throw new Error('No apiKey provided')
 
-    this.config = require('./config.json');
-    this.engine = require('got');
-    this.apiKey = apiKey;
+    this.config = require('./config.json')
+    this.engine = require('got')
+    this.apiKey = apiKey
   }
 
   build(opts, type) {
-    const urlParts = [];
+    const urlParts = []
     for (let option in opts) {
       if (this.config.map[type][option]) {
-        urlParts.push(this.config.map[type][option] + '=' + opts[option]);
+        urlParts.push(this.config.map[type][option] + '=' + opts[option])
       } else {
-        return;
+        return
       }
     }
-    return urlParts.join('&');
+    return urlParts.join('&')
   }
 
   format(obj) {
     if (typeof obj === 'object') {
-      const newObj = {};
+      const newObj = {}
       for (let i in obj) {
-        newObj[i.toLowerCase()] = this.format(obj[i]);
+        newObj[i.toLowerCase()] = this.format(obj[i])
       }
-      return newObj;
+      return newObj
     } else if (typeof obj === 'string' && obj === 'N/A') {
-      obj = null;
+      obj = null
     }
-    return obj;
+    return obj
   }
 
   error(opts, type) {
     if (type === 'search' && !opts.search) {
-      throw new Error('You need to pass a search query');
+      throw new Error('You need to pass a search query')
     } else if (type === 'get' && !opts.id && !opts.title) {
-      throw new Error('You need to pass an id or a title');
+      throw new Error('You need to pass an id or a title')
     }
   }
 
   verify(response) {
     if (response.body.Error) {
-      throw new Error(response.body.Error);
+      throw new Error(response.body.Error)
     }
-    return response;
+    return response
   }
 
   searchParse(response) {
-    const data = response.body.Search;
-    const res = {};
+    const data = response.body.Search
+    const res = {}
     for (let i in data) {
-      res[i] = {};
+      res[i] = {}
       for (let j in data[i]) {
-        res[i][j] = data[i][j];
+        res[i][j] = data[i][j]
       }
     }
-    return this.format(res);
+    return this.format(res)
   }
 
   getParse (response) {
-    const data = response.body;
-    const res = {};
+    const data = response.body
+    const res = {}
     for (let i in data) {
       if (['Actors', 'Genre', 'Writer', 'Director'].indexOf(i) !== -1) {
-        res[i] = data[i].split(', ');
+        res[i] = data[i].split(', ')
       } else if (i === 'Response') {
         // do nothing
       } else {
-        res[i] = data[i];
+        res[i] = data[i]
       }
     }
-    return this.format(res);
+    return this.format(res)
   }
 
   get(opts, type, count) {
-    this.error(opts, type);
+    this.error(opts, type)
 
-    count = isNaN(count) ? 1 : count;
+    count = isNaN(count) ? 1 : count
 
     return this.engine(this.config.url + this.build(opts, type) + `&apikey=${this.apiKey}`, {
       json: true
@@ -86,11 +86,11 @@ module.exports = class UTILS {
             this[type+'Parse'](this.verify(response))
            ).catch(error => {
       if (count < this.config.retries) {
-        count++;
-        return this.get(opts, type, count);
+        count++
+        return this.get(opts, type, count)
       } else {
-        throw error;
+        throw error
       }
-    });
+    })
   }
-};
+}
